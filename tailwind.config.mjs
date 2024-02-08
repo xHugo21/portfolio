27e01089ef8574d6ab16d455d38a9918041d5d54
Hugo García <55508123+xHugo21@users.noreply.github.com>
2024-02-08 14:31:16 +0100

    feat: added tailwind palette colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#DDE6ED",
+        secondary: "#9DB2BF",
+        tertiary: "#526D82",
+        quaternary: "#27374D",
+      },
+    },
+  },
+  plugins: [],
+};
