4717487644e27d6716de59b75314bedc7d6d4149
xHugo21 <hugo2121h@gmail.com>
2024-08-13 21:55:30 +0200

    feat: update color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            colors: {
-                customwhite: '#f2f2f2',
-                customblack: '#121212',
-                primary: '#542c6d',
-                secondary: '#b988d8',
-                accent: '#782caa',
-            },
-        },
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        customwhite: '#ebebd3',
+        customblack: '#1D1E2C',
+        primary: '#DDBDD5',
+        secondary: '#59656F',
+        tertiary: '#AC9FBB',
+      },
     },
-    plugins: [require('@tailwindcss/typography')],
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
